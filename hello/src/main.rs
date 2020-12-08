e703bbc7cc20cca335cbbf24fee38539c40820f3
qwerty2501 <qwerty2501@live.jp>
2020-12-09 01:12:40 +0900

    deployied to cloudrun.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,12 +3,17 @@ use std::env;
 
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", name, id)
+    format!("Hello CloudRun2 {}! id:{}", name, id)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = env::var("PORT").unwrap_or_else(|_|"8080".to_string());
+    let port = env::var("PORT").unwrap_or_else(|err|{
+        match err {
+            env::VarError::NotPresent => "8080".to_string(),
+            _ => panic!("unknown error"),
+        }
+    });
     HttpServer::new(|| App::new().service(index))
         .bind(format!("0.0.0.0:{}",port))?
         .run()
